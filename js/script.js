a82cd38380474e0620c534fea62e4c0045d36dec
Semicoder <125687040+SemicoderCS@users.noreply.github.com>
2023-03-31 20:13:42 +0430

    Header section completed

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// * Navbar mobile
+
+let bars = document.getElementById("bars");
+barsIcon = bars.getElementsByTagName('span');
+let mobileMenu = document.getElementById("mobile-menu");
+
+console.log(barsIcon)
+let isOpen = false;
+bars.addEventListener('click', () => {
+    if (isOpen) {
+        mobileMenu.classList.remove('-right-0');
+        mobileMenu.classList.add('-right-[250px]');
+        barsIcon[0].classList.remove('hidden');
+        barsIcon[1].classList.add('hidden');
+        isOpen = false;
+    } else {
+        mobileMenu.classList.remove('-right-[250px]');
+        mobileMenu.classList.add('-right-0');
+        barsIcon[0].classList.add('hidden');
+        barsIcon[1].classList.remove('hidden');
+        isOpen = true;
+    }
+})
