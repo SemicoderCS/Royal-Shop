14247866b9ec9bc09fd7cfdbf0f32899fe832d9b
Semicoder <125687040+SemicoderCS@users.noreply.github.com>
2023-04-02 22:14:12 +0430

    Top-Product section completed

--- js/script.js
+++ js/script.js
@@ -37,3 +37,33 @@ var swiper = new Swiper(".mySwiper", {
     },
 });
 
+// * Top Product Slider
+var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    freeMode: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: true,
+    },
+
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+        1280: {
+            slidesPerView: 5,
+            spaceBetween: 40,
+        },
+    },
+});
