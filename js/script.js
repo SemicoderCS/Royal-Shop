266c7c570c493adf5a08a21235fe44651f0dac42
Semicoder <125687040+SemicoderCS@users.noreply.github.com>
2023-04-08 23:06:28 +0430

    main page completed and shop page almost completed

--- js/script.js
+++ js/script.js
@@ -82,3 +82,32 @@ var swiper = new Swiper(".mySwiper3", {
     mousewheelControl: true,
     keyboardControl: true,
 });
+
+// * Brand Slider
+var swiper = new Swiper(".mySwiper4", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    // centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        570: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 5,
+            spaceBetween: 40,
+        },
+        1280: {
+            slidesPerView: 6,
+            spaceBetween: 40,
+        }
+    }
+});
